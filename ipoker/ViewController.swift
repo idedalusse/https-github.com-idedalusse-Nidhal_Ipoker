9f2730c31ec603cb3c081011299485499d10e1a5
idedalusse <31492548+idedalusse@users.noreply.github.com>
2017-10-16 14:31:29 -0400

    Initial Commit

---
+++ ipoker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ipoker
+//
+//  Created by BensNedalus on 16/10/2017.
+//  Copyright © 2017 BensNedalus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
