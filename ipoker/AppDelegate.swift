7452e459dd44b4a18db2e31bb1d9c4ed225ca920
idedalusse <31492548+idedalusse@users.noreply.github.com>
2017-10-26 13:25:56 -0400

    GoldMasteriPokerTPFINAL 26102017

--- ipoker/AppDelegate.swift
+++ ipoker/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        sleep(5)
+        sleep(5)// pour controller le launscreen un petit timer
         return true
     }
 
